f9612d381dd960a862083aa630ac825e0fb8f30f
arnold1945 <vscj58@gmail.com>
2025-10-25 13:59:13 -0500

    catchup

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,40 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    
+    output = ''
+    roman_dict = {
+        'I' : 1,
+        'IV' : 4,
+        'V' : 5,
+        'IX': 9,
+        'X' : 10,
+        'XIV' : 14,
+        'XLIV' : 44,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'CMXLIV' : 944,
+        'M' : 1000
+        }
+    
+
+    
+    roman_list = ['M','CMXLIV',  'D', 'C', 'L','XLIV','XIV', 'X','IX', 'V','IV', 'I']
+
+        
+    
+    for item in roman_list:
+        while num >= roman_dict[item]:
+        
+            output = output + item
+            num = num - roman_dict[item]
+
+            
+
+    return output
+    
+            
+
+
+
+    
+    
